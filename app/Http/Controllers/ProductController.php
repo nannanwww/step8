b39a5470d96728d45507d55ed6c007d9ebc25b87
nannanwww <engineerookie@gmail.com>
2023-11-23 16:20:55 +0900

    Commit message here

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,24 +10,14 @@ class ProductController extends Controller
 
     public function index(Request $request)
     {
-        $products = Product::query();
-        $companies = Product::pluck('company')->unique();
+        $productModel = new Product();
+        $companies = $productModel->pluck('company')->unique();
 
         $keyword = $request->input('key_word');
         $company = $request->input('key_company');
 
-        if ($keyword) {
-            $products = $products->where(function ($query) use ($keyword) {
-                $query->where('product_name', 'like', '%' . $keyword . '%')
-                    ->orWhere('description', 'like', '%' . $keyword . '%');
-            });
-        }
+        $products = $productModel->searchProducts($keyword, $company);
 
-        if ($company) {
-            $products = $products->where('company', $company);
-        }
-
-        $products = $products->orderBy('product_name')->paginate(6);
         $request->session()->put('products_page', $products->currentPage());
         $request->session()->put('searchParams', $request->getQueryString());
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,65 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['id', 'image', 'product_name', 'price', 'stock', 'company'];
+
+    public function searchProducts($keyword, $company)
+    {
+        $query = $this->query();
+
+        if ($keyword) {
+            $query->where(function ($query) use ($keyword) {
+                $query->where('product_name', 'like', '%' . $keyword . '%')
+                    ->orWhere('description', 'like', '%' . $keyword . '%');
+            });
+        }
+
+        if ($company) {
+            $query->where('company', $company);
+        }
+
+        return $query->orderBy('product_name')->paginate(6);
+    }
+
+    public static function updateProduct(Request $request, $id)
+    {
+        $product = self::find($id);
+
+        if (!$product) {
+            return null; // 商品が見つからない場合の処理
+        }
+
+        $request->validate([
+            'product_name' => 'required|max:20',
+            'company' => 'required|max:20',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'description' => 'nullable|max:140',
+            'image' => 'nullable|image|max:2048',
+        ]);
+
+        // 更新処理
+        $product->product_name = $request->input("product_name");
+        $product->company = $request->input("company");
+        $product->price = $request->input("price");
+        $product->stock = $request->input("stock");
+        $product->description = $request->input("description");
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('public/images', $imageName);
+            $product->image = 'storage/images/' . $imageName;
+        }
+
+        $product->save();
+
+        return $product;
+    }
 }
